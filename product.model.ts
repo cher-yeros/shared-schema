ab85d0e46f7398a27fc13784fcf5909f7c249467
Yerosen Diriba <yerosendiriba1@gmail.com>
2023-10-19 10:42:59 +0300

    updated models

--- product.model.ts
+++ product.model.ts
@@ -35,15 +35,12 @@ class Product extends Model<ProductType, ProductCreationAttributes> {
     type: DataType.STRING,
     allowNull: false,
   })
-<<<<<<< HEAD
-=======
   serialNo!: string;
 
   @Column({
     type: DataType.STRING,
     allowNull: false,
   })
->>>>>>> c60da7b0fca1e9f7b3c56b0dfc125939854784be
   name!: string;
 
   @Column({
@@ -61,6 +58,7 @@ class Product extends Model<ProductType, ProductCreationAttributes> {
   @ForeignKey(() => User)
   @Column({
     type: DataType.UUID,
+    allowNull: false,
   })
   userId!: string;
 
